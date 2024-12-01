4d6417f567457fc5ed1813fc7f3a022e064e3988
hansen126 <hansen126187@gmail.com>
2024-12-02 00:03:03 +0700

    Change UI Colors

--- app/src/main/java/com/example/playconsign/MainActivity.java
+++ app/src/main/java/com/example/playconsign/MainActivity.java
@@ -1,24 +1,58 @@
 package com.example.playconsign;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.playconsign.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding.mainBottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int selectedId = item.getItemId();
+                if(selectedId == R.id.mainHomeMenu){
+                    switchFragment(new HomeFragment());
+                } else if (selectedId == R.id.mainConsignMenu) {
+                    switchFragment(new ConsignFragment());
+                } else if (selectedId == R.id.mainProfileMenu) {
+                    switchFragment(new ProfileFragment());
+                }
+                return true;
+            }
+        });
+    }
+
+    private void switchFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.mainFCV, fragment);
+        fragmentTransaction.commit();
     }
 }
