83fc278f1b809ff0eaf9a6758a7960142d29ec2e
hansen126 <hansen126187@gmail.com>
2024-12-03 12:41:20 +0700

    Add Search Activity and adjust margin in Home

--- app/src/main/java/com/example/playconsign/SearchActivity.java
+++ app/src/main/java/com/example/playconsign/SearchActivity.java
@@ -1,6 +1,9 @@
 package com.example.playconsign;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,14 @@ public class SearchActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        ImageView backButton = findViewById(R.id.searchBackArrow);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
     }
 }
