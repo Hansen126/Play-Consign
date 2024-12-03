1104c73cb32358236ec6656e5c7343d55b6e9c38
hansen126 <hansen126187@gmail.com>
2024-12-03 16:47:36 +0700

    Add Cloudinary and Stop tracking gradle.properties

---
+++ app/src/main/java/com/example/playconsign/Product.java
@@ -0,0 +1,67 @@
+package com.example.playconsign;
+
+public class Product {
+    String productName;
+    int productPrice;
+    String productDesc;
+    String productImage;
+    String productCondition;
+    String productSeller;
+    String productSellerAddress;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(int productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductDesc() {
+        return productDesc;
+    }
+
+    public void setProductDesc(String productDesc) {
+        this.productDesc = productDesc;
+    }
+
+    public String getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(String productImage) {
+        this.productImage = productImage;
+    }
+
+    public String getProductCondition() {
+        return productCondition;
+    }
+
+    public void setProductCondition(String productCondition) {
+        this.productCondition = productCondition;
+    }
+
+    public String getProductSeller() {
+        return productSeller;
+    }
+
+    public void setProductSeller(String productSeller) {
+        this.productSeller = productSeller;
+    }
+
+    public String getProductSellerAddress() {
+        return productSellerAddress;
+    }
+
+    public void setProductSellerAddress(String productSellerAddress) {
+        this.productSellerAddress = productSellerAddress;
+    }
+}
