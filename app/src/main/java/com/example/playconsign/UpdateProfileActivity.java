98e4bc1ab75b6b06a16d6bfca71bc80eed919161
hansen126 <hansen126187@gmail.com>
2024-12-13 20:47:35 +0700

    Update UpdateProfileActivity

--- app/src/main/java/com/example/playconsign/UpdateProfileActivity.java
+++ app/src/main/java/com/example/playconsign/UpdateProfileActivity.java
@@ -8,7 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class UpdateProfileActivity extends AppCompatActivity {
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,9 @@ public class UpdateProfileActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        if (firebaseAuth.getCurrentUser() != null) {
+            String UID = firebaseAuth.getCurrentUser().getUid();
+        }
     }
 }
